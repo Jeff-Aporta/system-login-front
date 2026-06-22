b01774e0239029a5215b47293102668a580e5c90
Jeff-Aporta <jeffaporta@gmail.com>
2026-06-22 10:29:16 -0500

    chore: Se actualiza el pin de front-shared en el arranque del front
    Tras sync:all-versions el index y loader referencian el commit canónico publicado en jsDelivr para mantener coherencia con el resto de aplicaciones del monorepo.

--- js/boot/loader.ts
+++ js/boot/loader.ts
@@ -4,7 +4,7 @@
 (function () {
   "use strict";
 
-  const FRONT_SHARED_REF = "846b658";
+  const FRONT_SHARED_REF = "13629aa";
   const isLocalDev = /localhost|127\.0\.0\.1|\[::1\]/.test(location.hostname);
   const BOOT_HELPER = isLocalDev
     ? "../../components/front-shared/cdn/boot-helper.mjs"
