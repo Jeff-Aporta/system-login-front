a2a1d9f596a173254cb3359ac313c8d7e437ba3a
Jeff-Aporta <jeffaporta@gmail.com>
2026-06-22 13:49:58 -0500

    chore: Sincroniza pin front-shared y arranque tras sync de versiones

--- js/boot/loader.ts
+++ js/boot/loader.ts
@@ -4,7 +4,7 @@
 (function () {
   "use strict";
 
-  const FRONT_SHARED_REF = "13629aa";
+  const FRONT_SHARED_REF = "7c00390";
   const isLocalDev = /localhost|127\.0\.0\.1|\[::1\]/.test(location.hostname);
   const BOOT_HELPER = isLocalDev
     ? "../../components/front-shared/cdn/boot-helper.mjs"
