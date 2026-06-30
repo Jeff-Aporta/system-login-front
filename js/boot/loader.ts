c0bcd632283da9da68c12e250ac3f465c0bd220c
Jeff-Aporta <jeffaporta@gmail.com>
2026-06-30 08:00:16 -0500

    chore(cdn): sincronizar pins a release-2026-06-30 ( js/boot/loader.ts)

--- js/boot/loader.ts
+++ js/boot/loader.ts
@@ -4,7 +4,7 @@
 (function () {
   "use strict";
 
-  const FRONT_SHARED_REF = "33acc67";
+  const FRONT_SHARED_REF = "release-2026-06-30";
   const isLocalDev = /localhost|127\.0\.0\.1|\[::1\]/.test(location.hostname);
   const BOOT_HELPER = isLocalDev
     ? "../../components/front-shared/cdn/boot-helper.mjs"
